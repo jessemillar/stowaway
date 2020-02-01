0eb41fedf866ad872195e0c27dd240cc3794c67a
Jesse Millar <jesse.millar@microsoft.com>
2020-02-01 16:13:25 -0600

    Built in Fitbit Studio because of iPad limitations
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- app/index.js
+++ app/index.js
@@ -1,76 +1,48 @@
 import clock from "clock";
-import { today } from "user-activity";
 import document from "document";
+import { preferences } from "user-settings";
 import * as util from "../common/utils";
+import { today } from "user-activity";
 
+// Update the clock every minute
 clock.granularity = "minutes";
 
-// Time digit positions
-let pos1 = { x: -4, y: 10 };
-let pos2 = { x: 27, y: 4 };
-let pos3 = { x: 51, y: 4 };
-let pos4 = { x: 76, y: 2 };
-let pos5 = { x: 109, y: 0 };
-let pos1b = { x: 12, y: 4 };
-let pos2b = { x: 36, y: 4 };
-let pos3b = { x: 61, y: 2 };
-let pos4b = { x: 94, y: 0 };
-
-let day = document.getElementById("day");
-
-let dateDigits = [
-  document.getElementById("date-1"),
-  document.getElementById("date-2"),
-  document.getElementById("date-3"),
-  document.getElementById("date-4"),
-  document.getElementById("date-5")
+const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec"
 ];
 
-let stepDigits = [
-  document.getElementById("steps-1"),
-  document.getElementById("steps-2"),
-  document.getElementById("steps-3"),
-  document.getElementById("steps-4"),
-  document.getElementById("steps-5"),
-  document.getElementById("steps-6")
-];
+// Get a handle on the <text> element
+const hoursLabel = document.getElementById("hoursLabel");
+const minutesLabel = document.getElementById("minutesLabel");
+const stepsLabel = document.getElementById("stepsLabel");
+const dateLabel = document.getElementById("dateLabel");
 
-let colon = document.getElementById("colon");
-let hourLeft = document.getElementById("hour-left");
-let hourRight = document.getElementById("hour-right");
-let minuteLeft = document.getElementById("minute-left");
-let minuteRight = document.getElementById("minute-right");
-
+// Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let date = evt.date;
-  let curDay = date.getDay();
-  let curHour = util.zeroPad(date.getHours() % 12 || 12);
-  let curMinute = util.zeroPad(date.getMinutes());
-
-  util.setDayValue(day, curDay);
-  util.drawDate(dateDigits, date.getMonth(), date.getDate());
-  util.drawSteps(stepDigits, today.local.steps || 0);
-
-  if (curHour > 9) {
-    hourLeft.style.visibility = "visible";
-    util.setLargeDigitValue(hourLeft, ("" + curHour)[0]);
-
-    util.setDigitPosition(hourLeft, pos1);
-    util.setDigitPosition(hourRight, pos2);
-    util.setDigitPosition(colon, pos3);
-    util.setDigitPosition(minuteLeft, pos4);
-    util.setDigitPosition(minuteRight, pos5);
+  let hours = date.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
   } else {
-    hourLeft.style.visibility = "hidden";
-
-    util.setDigitPosition(hourRight, pos1b);
-    util.setDigitPosition(colon, pos2b);
-    util.setDigitPosition(minuteLeft, pos3b);
-    util.setDigitPosition(minuteRight, pos4b);
+    // 24h format
+    hours = util.zeroPad(hours);
   }
+  let mins = util.zeroPad(date.getMinutes());
 
-  util.setLargeDigitValue(hourRight, ("" + curHour)[1]);
-
-  util.setLargeDigitValue(minuteLeft, ("" + curMinute)[0]);
-  util.setLargeDigitValue(minuteRight, ("" + curMinute)[1]);
+  hoursLabel.text = `${hours}`;
+  minutesLabel.text = `${mins}`;
+  dateLabel.text = monthNames[date.getMonth()] + " " + date.getDate();
+  stepsLabel.text = today.local.steps || 0;
 }
